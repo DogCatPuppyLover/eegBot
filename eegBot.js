bac794c36147c0d1d960403ae3d87d4d407496c6
Isla <79656307+DogCatPuppyLover@users.noreply.github.com>
2021-02-25 12:03:58 -0800

    Update eegBot.js

--- eegBot.js
+++ eegBot.js
@@ -18,15 +18,13 @@ var splitCommand;
 
 var topCommentId = 0;
 
-var append = " | This message was performed automatically. If the bot is malfunctioning, please stop the bot by posting '/eeg stop' and contact @DogCatPuppyLover. RNTAS: "
+var append = " | RNTAS: "
 
-function send(message, commentUser, append, commentId, studio_id, n) {
+function send(message, commentUser, commentId, studio_id, n) {
   $.getJSON("https://api.scratch.mit.edu/users/" + commentUser, function(data) {
     message = "Welcome! If you haven't already been invited, you'll be invited soon."; //set message
-    if (commentId > topCommentId) { //prevent spam
-      console.log(message + append + commentId, commentId, data.id, studio_id);
-      postComment(message + append + commentId, commentId, data.id, studio_id); //welcome curator
-    }
+    console.log(message + append + commentId, commentId, data.id, studio_id);
+    postComment(message + append + commentId, commentId, data.id, studio_id); //welcome curator
   });
 }
 
@@ -51,27 +49,34 @@ function inviteCurator(username, studio_id) {
 
 function bot(studio_id) {
   console.log("Running bot");
-  $.ajax({ //get comment data
+  $.ajax({
     url: "https://scratch.mit.edu/site-api/comments/gallery/" + studio_id,
     success: function(data) {
-      domparser = new DOMParser(); //parse string into DOM
+      domparser = new DOMParser();
       doc = domparser.parseFromString(data, "text/html");
-      for (n = 0; n < doc.getElementsByClassName("top-level-reply").length; n++) { //check every comment on the first page
-        topLevel = doc.getElementsByClassName("top-level-reply")[n]; //nth top-level comment
+      for (n = 0; n < doc.getElementsByClassName("top-level-reply").length; n++) {
+        topLevel = doc.getElementsByClassName("top-level-reply")[n];
         checkComment = topLevel.getElementsByClassName("content")[0].innerHTML.toLowerCase().trim();
         commentId = topLevel.getElementsByTagName("div")[0].getAttribute("data-comment-id");
         commentUser = topLevel.getElementsByTagName("a")[1].innerHTML;
         splitCommand = checkComment.split(" ");
         console.log(checkComment + ", " + commentId + ", " + commentUser);
-        if (splitCommand[0] === "/eeg") {
-          if (splitCommand[1] === "stop") { //stop command
-            setTimeout(send("Stopping bot.", commentUser, append, commentId, commentId, studio_id, n), 2000);
-            clearInterval(bot)
-          } else if (splitCommand[1] === "invite") { //invite curator
-            setTimeout(send("Welcome! If you haven't already been invited, you'll be invited soon.", commentUser, append, commentId, studio_id, n), 2000);
-            inviteCurator(commentUser, studio_id); //invite curators
-          } else {
-            setTimeout(send("Sorry, '" + splitCommand[1] + "' is not a command.", commentUser, append, commentId, studio_id, n), 2000);
+        if (commentId > topCommentId) {
+          if (splitCommand[0] === "/eeg") {
+            if (splitCommand[1] === "stop") {
+              setTimeout(send("Stopping bot.", commentUser, commentId, commentId, studio_id, n), 2000);
+              clearInterval(bot)
+            } else if (splitCommand[1] === "invite") {
+              setTimeout(send("Welcome to chaos! If you haven't already been invited, you'll be invited soon.", commentUser, commentId, studio_id, n), 2000);
+              inviteCurator(commentUser, studio_id)
+            } else if (splitCommand[1] === "huggles") {
+              setTimeout(send("*huggles*", commentUser, commentId, studio_id, n), 2000);
+            } else {
+              setTimeout(send("Whoops! '" + splitCommand[1] + "' is not a command.", commentUser, commentId, studio_id, n), 2000);
+            }
+          }
+          if (checkComment.includes("[delete]") || checkComment.includes("[remove]") || checkComment.includes("(delete)") || checkComment.includes("(remove)")) {
+              setTimeout(send("Please don't try to bypass the filterbot or send links to websites with unmoderated chat.", commentUser, commentId, studio_id, n), 2000);
           }
         }
         if (n == 0) {
@@ -84,4 +89,4 @@ function bot(studio_id) {
 
 setInterval(function() {
   bot("5842709");
-}, 300000); //run the script every 10 minutes
+}, 300000);
